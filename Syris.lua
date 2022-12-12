8521f98d5de930d7944204d279d3a74518cda8b1
a <105553346+arabnoob@users.noreply.github.com>
2022-12-12 15:25:37 +0300

    Update Syris.lua

--- Syris.lua
+++ Syris.lua
@@ -1,1 +1,36 @@
+--Booting Up
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local uh = OrionLib:MakeWindow({Name = "Unknown Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionLib"})
 
+--Tabs
+local main = uh:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})()
+
+--Sections
+local inf = main:AddSection({
+	Name = "Unknown"
+})
+
+--Buttons
+main:AddButton({
+	Name = "Execute Infinite Yield",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+  	end    
+})
+
+main:AddButton({
+	Name = "Stream Sniper",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/Miscellaneous/main/StreamSniper.lua"))()
+  	end    
+})
+main:AddButton({
+	Name = "Rapper tycoon script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraIsMe2/ToraIsMe2/main/0mom"))()
+  	end    
+})
